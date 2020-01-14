2bc4b0a9ca017d8ea4cd38472733d1c38814a18e
Carlos Feldmann <feldmannjunior@gmail.com>
2020-01-14 19:13:28 -0300

    Creating parent pom

--- ci/nexus/initialize/src/main/groovy/security.groovy
+++ ci/nexus/initialize/src/main/groovy/security.groovy
@@ -1,8 +1,8 @@
-def security = getSecurity();
+//def security = getSecurity();
 security.setAnonymousAccess(false)
 
 security.addRole(
-        "deployer",
+        "deployer-role",
         "Deployer",
         "Role with the privileges to deploy to repositories",
         [
@@ -11,21 +11,55 @@ security.addRole(
         ['nx-anonymous']
 )
 security.addRole(
-        "developer",
+        "developer-role",
         "Developer",
         "Role with the privileges to read from repositories",
         [
                 'nx-repository-view-*-*-read',
                 'nx-repository-view-*-*-browse',
+                'nx-repository-view-docker-*-add',
+                
         ],
         ['nx-anonymous']
 )
+security.addRole(
+        "k8s-role",
+        "Kubernetes Role",
+        "Role with the privileges to read from docker repository",
+        [
+                'nx-repository-view-docker-*-read',
+                'nx-repository-view-docker-*-browse',
+        ],
+        ['nx-anonymous']
+)
+// User to k8s pull images for pods
+security.addUser(
+        'k8s',
+        'Kubernetes',
+        'User',
+        'kubernetes@constellation.feldmann.dev',
+        true,
+        "${NEXUS_K8S_PASSWORD}",
+        ['k8s-role']
+);
+// Developer user to read maven repositories and push docker images
+security.addUser(
+        'developer',
+        'Developer',
+        'User',
+        'developer@constellation.feldmann.dev',
+        true,
+        "${NEXUS_DEVELOPER_PASSWORD}",
+        ['developer-role']
+)
+
+// Jenkins user to push images to repository
 security.addUser(
         "jenkins-deployer",
         "Deployer",
         "Jenkins",
-        "jenkins@example.com",
+        "jenkins@constellation.feldmann.dev",
         true,
         "$NEXUS_DEPLOYER_PASSWORD",
-        ['deployer']
+        ['deployer-role']
 )
